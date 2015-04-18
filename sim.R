ef4e176a094ca2ed057c67b231ede81e37279cda
Alex Yakubovich <alex.yakubovich@gmail.com>
2015-04-18 17:23:00 -0400

    remove reference to my working directory

--- sim.R
+++ sim.R
@@ -4,9 +4,8 @@
 
 # part 1) generate a sample from a gaussian mixture model, corrupt it with noise and fit the GMM usingt the EM algorithm
 
-# part 2) generate a sequence of observations from a hidden markov model, corrupt it with noise and fit the HMM usingt the EM algorithm for HMM. A 3rd party implementation is used to compute the responsibilities in the E-step (the RHmm package)
-
-setwd("/Users/alexyakubovich/Desktop/6631/Project")
+# part 2) generate a sequence of observations from a hidden markov model, corrupt it with noise and fit the HMM usingt the EM algorithm for HMM. 
+# The RHmm package is used to compute the responsibilities in the E-step
 
 # 1) mixture model
 source("genMM.r")
@@ -23,4 +22,3 @@ source("fitHMM.r")
 y2 <- genHMM(N=100, vis=1)
 y2 <- y2 + rmvnorm(100, mean=c(0,0), sigma=diag(2)) # corrupt with iid zero mean isotropic gaussian noise
 fitHMM(y2, maxIt=200)
-#lines(ellipse(sigma,centre=mu[2,], level=c(0.95, .9), col='green')
